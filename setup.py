5cab6a352b9bc329ee3c229c6cc0d329c12db0a5
ChristophBeckmann <45755967+ChristophBeckmann@users.noreply.github.com>
2022-02-06 17:58:44 +0100

    Initial Setup of Files

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
-print("Test")
-print("Test2")
-print("Test3")
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    licensefile = f.read()
+
+setup(
+    name='ResearchTool',
+    version='0.1.0',
+    description='A Tool to summarize text, get keywords and analytics keywords',
+    long_description=readme,
+    author='Christoph Beckmann',
+    author_email='Christoph-Beckmann@outlook.de',
+    url='https://github.com/ChristophBeckmann/ResearchTool',
+    license=licensefile,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
